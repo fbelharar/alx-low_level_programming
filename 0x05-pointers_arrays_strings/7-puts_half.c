0d7f62ec2927349e4f12f4ab86c235f8853d94a4
fbelharar <fatimazahra.belharar@gmail.com>
2023-03-27 16:19:21 -0700

    ex7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,14 +15,13 @@ void puts_half(char *str)
 			break;
 		count++;
 	}
-	
 
 	if (count % 2 == 1)
 		i = count / 2;
 	else
 		i = (count - 1) / 2;
 
-	for ( i++ ; i < count; i++)
+	for (i++; i < count; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
